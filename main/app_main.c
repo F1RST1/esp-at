53adee5dbfe5b1b964c1def84483b61cdb38363d
F1RST1 <47220608+F1RST1@users.noreply.github.com>
2024-02-23 08:56:30 +0000

    [add] cmd regist

--- main/app_main.c
+++ main/app_main.c
@@ -61,3 +61,42 @@ static void example_event_callback(esp_blufi_cb_event_t event, esp_blufi_cb_para
         break;
     }
 }
+
+
+
+uint8_t at_testCmd(uint8_t *cmd_name)
+{
+    _print_to_at_string("at_testCmd\r\n");
+    return ESP_AT_RESULT_CODE_OK;
+}
+
+uint8_t at_queryCmd(uint8_t *cmd_name)
+{
+    _print_to_at_string("at_queryCmd\r\n");
+    return ESP_AT_RESULT_CODE_OK;
+}
+
+uint8_t at_setupCmd(uint8_t para_num)
+{
+    _print_to_at_string("at_setupCmd\r\n");
+    return ESP_AT_RESULT_CODE_OK;
+}
+
+uint8_t at_exeCmd(uint8_t *cmd_name)
+{
+    _print_to_at_string("at_exeCmd\r\n");
+    const char* to_at_port = "from_github_action";
+    esp_blufi_send_custom_data_p2e((uint8_t *)to_at_port, strlen(to_at_port));
+    return ESP_AT_RESULT_CODE_OK;
+}
+
+static const esp_at_cmd_struct at_uart_cmd[] = {
+    {"+UBLUFISEND", at_testCmd, at_queryCmd, at_setupCmd, at_exeCmd},
+};
+
+bool esp_at_ublufi_cmd_regist(void)
+{
+    return esp_at_custom_cmd_array_regist(at_uart_cmd, sizeof(at_uart_cmd) / sizeof(esp_at_cmd_struct));
+}
+
+ESP_AT_CMD_SET_FIRST_INIT_FN(esp_at_ublufi_cmd_regist);
